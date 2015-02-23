1f73e9e4c6a50e8574407e4b24376412c80c5d0e
stu157 <stu157@gmail.com>
2015-02-23 15:23:43 +0100

    NoteController-Merge

--- Notizverwaltung/src/application/note/NoteController.java
+++ Notizverwaltung/src/application/note/NoteController.java
@@ -9,14 +9,19 @@ import com.sun.glass.ui.Pixels.Format;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 public class NoteController implements Initializable
 {
@@ -28,19 +33,16 @@ public class NoteController implements Initializable
 		if(parentController == null)
 			parentController = mc;
 	}
-	
-	public void setSelectedNote(Note newNote)
-	{
+	public void setSelectedNote(Note newNote) {
 		selectedNote = newNote;
 		noteContent.setText(selectedNote.getContent());
 		title.setText(selectedNote.getTitle());
 	}
-	
-	public Note getSelectedNote()
-	{
+
+	public Note getSelectedNote() {
 		return selectedNote;
 	}
-	
+
 	//ChangeListener für TextArea noteContent
 	private void setContentChangeListener()
 	{
@@ -67,27 +69,61 @@ public class NoteController implements Initializable
 	@FXML
 	private Button newUrl;
 	@FXML
-    private ListView<Hyperlink> noteUrlList;
-    @FXML
-    private TextArea noteContent;
-    @FXML
-    private TextField title;
-    @FXML
-    private ListView<Image> noteImagesList;
-    
-    public String getContent()
-    {
-    	return noteContent.getText();
-    }
-    
-    public String getTitle()
-    {
-    	return title.textProperty().get();
-    }	
-    
+	private ListView<Hyperlink> noteUrlList;
+	@FXML
+	private TextArea noteContent;
+	@FXML
+	private TextField title;
+	@FXML
+	private ListView<Image> noteImagesList;
+
+	public String getContent() {
+		return noteContent.getText();
+	}
+
+	public String getTitle() {
+		return title.textProperty().get();
+	}
+
+	@FXML
+	void newImageCommand(ActionEvent event) {
+		try {
+			Parent root = FXMLLoader.load((getClass()
+					.getResource("/application/path/Image.fxml")));
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.setTitle("Neues Bild hinzufügen");
+			// Setzt das ProgrammIcon
+			stage.getIcons().add(
+					new Image("application/images/Icon.png"));
+
+			stage.setResizable(false);
+			stage.show();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@FXML
+	void newUrlCommand(ActionEvent event) {
+		try {
+			Parent root = FXMLLoader.load((getClass()
+					.getResource("/application/url/Url.fxml")));
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.setTitle("Neuer URL hinzufügen");
+			// Setzt das ProgrammIcon
+			stage.getIcons().add(
+					new Image("application/images/Icon.png"));
+			stage.setResizable(false);
+			stage.show();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
 	@Override
-	public void initialize(java.net.URL arg0, ResourceBundle arg1) 
-	{
+	public void initialize(java.net.URL arg0, ResourceBundle arg1) {
 		setContentChangeListener();
 	}
 }
